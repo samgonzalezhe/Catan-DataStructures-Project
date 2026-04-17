91493f0d3fffa3f57013cf1e4839db0492ded763
samgonzalezhe <samgonzalezhe@unal.edu.co>
2026-04-17 17:57:40 -0500

    Agregado Lista de vertice y aristas por hexagono y sus metodos

--- src/model/Hexagono.java
+++ src/model/Hexagono.java
@@ -1,14 +1,50 @@
-package com.mycompany.catan;
+package model;
+
+import logic.Recurso;
+import logic.TipoTerreno;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Hexagono {
-    
-    private TipoTerreno tipo;
 
-        public TipoTerreno getTipo() {
-            return tipo;
-        }
-        
-        
+    private TipoTerreno tipo;
+    public int numero;
+    boolean tieneLadron;
+    public HexCoord coord;
+    private List<Vertice> vertices;
+    private List<Arista> aristas;
+
+    public Hexagono(TipoTerreno tipo, HexCoord coord) {
+        this.tipo = tipo;
+        this.coord = coord;
+        this.numero = 0;
+        this.tieneLadron = false;
+        this.vertices = new ArrayList<>();
+        this.aristas = new ArrayList<>();
+    }
+
+    @Override
+    public String toString() {
+        return coord + " -> " + tipo +
+                (tipo != TipoTerreno.DESIERTO ? " (" + numero + ")" : " [LADRÓN]");
+    }
+
+    public boolean tieneLadron() {
+        return tieneLadron;
+    }
+
+    public void agregarVertice(Vertice v) { this.vertices.add(v); }
+    public void agregarArista(Arista a) { this.aristas.add(a); }
+
+    //Getters y setters
+    public TipoTerreno getTipo() {
+        return tipo;
+    }
+
+    public List<Vertice> getVertices() { return vertices; }
+    public List<Arista> getAristas() { return aristas; }
+
     public Recurso getRecurso() {
 
         switch (tipo) {
@@ -26,30 +62,12 @@ public class Hexagono {
                 return null; // desierto
         }
     }
-    int numero; // 2–12 (excepto desierto)
-    boolean tieneLadron;
-    HexCoord coord;
-
-    public Hexagono(TipoTerreno tipo, HexCoord coord) {
-        this.tipo = tipo;
-        this.coord = coord;
-        this.numero = 0;
-        this.tieneLadron = false;
-    }
-
-    @Override
-    public String toString() {
-        return coord + " -> " + tipo +
-                (tipo != TipoTerreno.DESIERTO ? " (" + numero + ")" : " [LADRÓN]");
-    }
-    
-
-    public boolean tieneLadron() {
-        return tieneLadron;
-    }
 
     public void setLadron(boolean valor) {
         this.tieneLadron = valor;
     }
-    
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
 }
