2b457e1d4bedc802cea63e3d068cece7fa73b6fd
samgonzalezhe <samgonzalezhe@unal.edu.co>
2026-04-17 18:00:17 -0500

    Añadidos metodos a Arista

--- src/model/Arista.java
+++ src/model/Arista.java
@@ -1,4 +1,6 @@
-package com.mycompany.catan;
+package model;
+
+import logic.Jugador;
 
 import java.util.List;
 
@@ -27,40 +29,43 @@ public class Arista {
             System.out.println("Ya hay una carretera en esta arista");
         }
     }
-    
-    public int getId() {
-        return id;
-    }
-    
-    public Vertice getV1() {
-        return v1;
-    }
 
-    public Vertice getV2() {
-        return v2;
-    }
-    
-    public Construccion getConstruccion() {
-        return construccion;
-    }
-    
-    public boolean puedeConstruirCarretera(Jugador jugador, List<Arista> aristas) {
+    public boolean construirCarreteraInicial(Jugador jugador) {
 
         if (this.carretera != null) {
             return false;
         }
-        
+        // Debe estar conectada a la aldea recién puesta
+        if (v1.tieneConstruccionDe(jugador) ||
+                v2.tieneConstruccionDe(jugador)) {
+
+            this.carretera = new Carretera(jugador);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean puedeConstruirCarretera(Jugador jugador, List<Arista> aristas) {
+
+        // 1. Debe estar libre
+        if (this.carretera != null) {
+            return false;
+        }
+
+        // 2. Conectada a construcción del jugador
         if (v1.tieneConstruccionDe(jugador) || v2.tieneConstruccionDe(jugador)) {
             return true;
         }
 
+        // 3. Conectada a otra carretera del jugador
         for (Arista a : aristas) {
 
             if (a.carretera != null &&
-                a.carretera.getPropietario() == jugador) {
+                    a.carretera.getPropietario() == jugador) {
 
+                // Si comparte vértice con esta arista
                 if (a.v1 == v1 || a.v1 == v2 ||
-                    a.v2 == v1 || a.v2 == v2) {
+                        a.v2 == v1 || a.v2 == v2) {
 
                     return true;
                 }
@@ -69,48 +74,30 @@ public class Arista {
 
         return false;
     }
-    
-    public boolean construirCarretera(Jugador jugador, List<Arista> aristas) {
 
-        if (puedeConstruirCarretera(jugador, aristas)) {
-            this.carretera = new Carretera(jugador);
-            return true;
-        }
 
-        return false;
-    }
-    
-    public boolean construirCarreteraInicial(Jugador jugador) {
+    //Getters y setters
 
-        if (this.carretera != null) {
-            return false;
-        }
-    // Debe estar conectada a la aldea recién puesta
-        if (v1.tieneConstruccionDe(jugador) ||
-            v2.tieneConstruccionDe(jugador)) {
-            
-            this.carretera = new Carretera(jugador);
-            return true;
-        }
-        return false;
-    }
-
-    public boolean estaConectadaAJugador(Jugador jugador) {
-
-        if (v1.getConstruccion() != null &&
-            v1.getConstruccion().getPropietario() == jugador) {
-            return true;
-        }
-
-        if (v2.getConstruccion() != null &&
-            v2.getConstruccion().getPropietario() == jugador) {
-            return true;
-        }
-
-        return false;
-    }
-    
     public void setConstruccion(Construccion construccion) {
         this.construccion = construccion;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public Vertice getV1() {
+        return v1;
+    }
+
+    public Vertice getV2() {
+        return v2;
+    }
+
+    public Construccion getConstruccion() {
+        return construccion;
+    }
+
+    public Carretera getCarretera() {return carretera; }
 }
+
