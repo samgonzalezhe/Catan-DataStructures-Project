044ee42afded6cd9f109f4c1d8e7165f1db6415b
samgonzalezhe <samgonzalezhe@unal.edu.co>
2026-04-17 16:56:11 -0500

    Costos Aldea

--- src/model/Aldea.java
+++ src/model/Aldea.java
@@ -2,6 +2,13 @@ package com.mycompany.catan;
 
 public class Aldea extends Construccion {
 
+    public static final Map<Recurso, Integer> COSTO = Map.of(
+            Recurso.MADERA, 1,
+            Recurso.LADRILLO, 1,
+            Recurso.TRIGO, 1,
+            Recurso.OVEJA, 1
+    );
+    
     public Aldea(Jugador propietario) {
         super(propietario);
     }
