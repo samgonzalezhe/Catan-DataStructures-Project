d21c55e40270132f235636b5438f36b2129cacb9
samgonzalezhe <samgonzalezhe@unal.edu.co>
2026-04-18 22:10:38 -0500

    Update Dado.java

--- src/java/model/Dado.java
+++ src/java/model/Dado.java
@@ -1,16 +1,22 @@
-package com.mycompany.catan;
+package logic;
 
 import java.util.Random;
 
 public class Dado {
-
     private Random random;
+    private int dado1;
+    private int dado2;
 
-    public Dado() {
+    public Dado(){
         random = new Random();
     }
 
-    public int lanzar() {
-        return random.nextInt(6) + 1;
+    public int lanzar(){
+        dado1 = random.nextInt(6) + 1;
+        dado2 = random.nextInt(6) + 1;
+        return dado1 + dado2;
     }
+
+    public int getDado1() { return dado1; }
+    public int getDado2() { return dado2; }
 }
