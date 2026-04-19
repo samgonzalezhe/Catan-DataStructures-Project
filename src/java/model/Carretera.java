489228e25d2b1d9c39133d727c0ab7408f3c0da2
D-G2 <digarciaf@unal.edu.co>
2026-04-19 14:04:41 -0500

    Update Carretera.java

--- src/java/model/Carretera.java
+++ src/java/model/Carretera.java
@@ -1,6 +1,13 @@
-package com.mycompany.catan;
+package model;
+
+import java.util.Map;
 
 public class Carretera extends Construccion {
+
+    public static final Map<Recurso, Integer> COSTO = Map.of(
+            Recurso.MADERA, 1,
+            Recurso.LADRILLO, 1
+    );
 
     public Carretera(Jugador propietario) {
         super(propietario);
