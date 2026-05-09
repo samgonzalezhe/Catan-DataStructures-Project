14155c7c7915ad0ead644ef726f624992ddc64ee
samgonzalezhe <samgonzalezhe@unal.edu.co>
2026-05-09 18:15:08 -0500

    Update Carretera.java

--- src/java/model/Carretera.java
+++ src/java/model/Carretera.java
@@ -1,20 +1,23 @@
 package model;
 
+import logic.Jugador;
+import logic.Recurso;
+
 import java.util.Map;
 
 public class Carretera extends Construccion {
+
+    public Carretera(Jugador propietario) {
+        super(propietario);
+    }
 
     public static final Map<Recurso, Integer> COSTO = Map.of(
             Recurso.MADERA, 1,
             Recurso.LADRILLO, 1
     );
 
-    public Carretera(Jugador propietario) {
-        super(propietario);
-    }
-
     @Override
     public int getPuntosVictoria() {
-        return 0; 
+        return 0;
     }
 }
